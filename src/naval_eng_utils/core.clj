88c8bab2052bb6e0f022a2b8db72293a40609e59
Adi Zhavo <adi@gram.gs>
2016-12-07 23:35:50 +0200

    Refactor the basic function that parses the csv file

--- src/naval_eng_utils/core.clj
+++ src/naval_eng_utils/core.clj
@@ -2,60 +2,29 @@
   (:require [clojure.java.io :as io])
   (:use clojure-csv.core))
 
-(def csv-file
+(def mapped-data (hash-map))
+
+(defn csv-file
+  [csv-name]
   (io/file
-  (io/resource "sampleReport.csv" ))) ; load the csv from the resurces
+  (io/resource csv-name))) ; load the csv from the resurces
 
-(def csv-raw-data
+(defn csv-raw-content
+  [csv]
   (parse-csv
-  (slurp csv-file) )) ; returns a sequence of vectors
+  (slurp csv))) ; returns a sequence of vectors
 
-(def csv-keys (atom ()))
-(def csv-data (atom ()))
-
-(defn build-csv-keys [csv-content]
-  (def temp-keys (atom ()))
+(defn csv-keys
+  [csv-content]
+  (def temp-keys (list))
   (doseq [element (nth csv-content 0)]
-  (swap! temp-keys conj (keyword element)) )
-  (swap! csv-keys conj (reverse (deref temp-keys))) )
-  ; setups the keys from the csv file
+    (def key-element (keyword element))
+    (def temp-keys (merge temp-keys key-element)))
+  (reverse (flatten temp-keys))) ; setups the keys from the csv file
 
-(defn build-csv-data [csv-content]
-  (doseq [element (nthrest csv-content 1)]
-  (swap! csv-data conj element) ))
-  ; get the content of the csv, will ignore the first row which are the key for the map
-
-(defn -main []
-  (build-csv-keys csv-raw-data)
-  (build-csv-data csv-raw-data)
-
-  (println (deref csv-keys))
-  (println (deref csv-data))
-
-  (doseq [data-set (deref csv-data)]
-  (doseq [element data-set]
-  (print element) (println (.indexOf data-set element)) ))
-  )
-
-; (defn -main []
-;   (println (map-indexed vector csv-list))
-;   (doseq [[x y z] csv-list]
-;   (println (vector (keyword x), (keyword y), (keyword z)) )
-;   (println x ) (println y) (println z))
-;   (println csv-list))
-
-; (defn -main []
-;   (println
-;     (map #(hash-map
-;       (keyword (for [x (first csv-list)] [x] ))
-;       ))))
-
-; (vec
-;   (rest %1 )))
-; )))
-; [(for [y x] [y] )] )))
-
-; (defn -main []
-;   (println
-;   (map #(hash-map (keyword (first %1)) (vec (rest %1))) csv-list ))
-;   (println csv-list ))
+(defn csv-data
+  [csv-content]
+  (def temp-data (list))
+    (doseq [element (nthrest csv-content 1)]
+    (def temp-data (merge temp-data element)))
+  (reverse temp-data)) ; get the content of the csv, will ignore the first row which are the key for the map
