a7a83e221e0365b65313b829d039fe38727f6fe2
Adi Zhavo <adi@gram.gs>
2016-12-08 08:46:35 +0200

    Update url and version in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject naval_eng_utils "0.1.0-SNAPSHOT"
+(defproject naval_eng_utils "0.0.1"
   :description "Utilities for naval engineers, handles csv manipulations, checks and reports"
-  :url "http://github.com/adizhavo"
+  :url "https://github.com/adizhavo/naval_eng_utils"
   :dependencies [[org.clojure/clojure "1.8.0"] [clojure-csv/clojure-csv "2.0.1"]]
-  :main naval-eng-utils.core)
+  :main naval-eng-utils.app)
