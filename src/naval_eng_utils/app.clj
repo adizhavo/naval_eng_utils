bd8242ffa348037db4a118c633260fce0677caf9
Adi Zhavo <adi@gram.gs>
2016-12-19 10:50:18 +0200

    Update app.clj

--- src/naval_eng_utils/app.clj
+++ src/naval_eng_utils/app.clj
@@ -1,5 +1,8 @@
 (ns naval-eng-utils.app
-  (:use naval-eng-utils.csv-databuilder))
+  (:use naval-eng-utils.csv-databuilder
+        [naval-eng-utils.action-provider :as reporter]))
 
 (defn -main []
-  (println (process-csv "sampleReport.csv")))
+  (println (reporter/execute (process-csv "sampleReport.csv")
+                             (process-csv "sampleReport1.csv")
+)))
