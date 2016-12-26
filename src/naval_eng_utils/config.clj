e0e30efd3be72db2c960a119f20663a82e20b85f
Adi Zhavo <adi@gram.gs>
2016-12-26 18:52:38 +0200

    Add cvs file names in the config file

--- src/naval_eng_utils/config.clj
+++ src/naval_eng_utils/config.clj
@@ -1,16 +1,29 @@
 (ns naval-eng-utils.config
   (:require clojure.core))
 
-(def checker-config {
+;file names config
+(def file-names-config {
+  :original "sampleReport.csv"
+  :test "sampleReport1.csv"
+})
+
+(def original-file (get file-names-config :original))
+(def test-file (get file-names-config :test))
+;------
+
+;Key search and action config
+(def action-config {
   :main-key (keyword "PIPING_CODE")
   :match-keys (list
     (keyword "DN")
     (keyword "ZONA")
 )})
+;------
 
-(def main-key (get checker-config :main-key))
-(def match-keys (get checker-config :match-keys))
+(def main-key (get action-config :main-key))
+(def match-keys (get action-config :match-keys))
 
+;Data builder default messages
 (def default-data {
   :empty_key "empty key"
   :empty_cell "n/a"
@@ -18,3 +31,4 @@
 
 (def empty-key (get default-data :empty-key))
 (def empty-cell (get default-data :empty-cell))
+;------
