c1deece390446a297ec1df7228f0b84f8b2783e7
Adi Zhavo <adi@gram.gs>
2016-12-27 15:52:36 +0200

    Add compilation options

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Utilities for naval engineers, handles csv manipulations, checks and reports"
   :url "https://github.com/adizhavo/naval_eng_utils"
   :dependencies [[org.clojure/clojure "1.8.0"] [clojure-csv/clojure-csv "2.0.1"]]
-  :main naval-eng-utils.app)
+  :main naval-eng-utils.app
+  :profiles {:uberjar {:aot :all}}
+  :aot [naval-eng-utils.app])
--- src/naval_eng_utils/app.clj
+++ src/naval_eng_utils/app.clj
@@ -1,7 +1,8 @@
 (ns naval-eng-utils.app
   (:require [naval-eng-utils.config :as config])
   (:use naval-eng-utils.csv-databuilder
-        [naval-eng-utils.action-executor :as op]))
+        [naval-eng-utils.action-executor :as op])
+  (:gen-class))
 
 (defn -main []
   (println (op/execute (build-csv-hashmap config/original-file)
