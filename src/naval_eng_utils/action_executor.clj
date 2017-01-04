8d89c8d243f09d625694929a9e154aa39fa098f3
Adi Zhavo <adi@gram.gs>
2017-01-04 11:05:30 +0200

    Add action provider

--- src/naval_eng_utils/action_executor.clj
+++ src/naval_eng_utils/action_executor.clj
@@ -1,21 +1,13 @@
 (ns naval-eng-utils.action-executor
-  (:require clojure.core [naval-eng-utils.error-handler :as error-check]
-    [naval-eng-utils.action.data-comparison :as actions]
-    [naval-eng-utils.config :as config]))
-
-(defn get-action-result
-  [action original-report test-report]
-  (cond (= action (get config/supported-action :comparison)) (actions/execute-comparison original-report test-report)
-        (= action (get config/supported-action :duplication)) (str "duplication action example.")
-        ;add conditions and actions here
-        :else (str "The inputed action: " action " is not supported.")
-))
+  (:require clojure.core
+    [naval-eng-utils.error-handler :as error-check]
+    [naval-eng-utils.action.action-provider :as action-provider]))
 
 (defn execute
-  "First checks if the reports are valid and executes the action"
+  "First checks if the reports are valid and requests the action result from the provider"
   [action original-report test-report]
   (def validity-message (error-check/check-reports-validity original-report test-report))
   (if (= 0 (count validity-message))
-      (get-action-result action original-report test-report)
+      (action-provider/get-result action original-report test-report)
       validity-message
 ))
