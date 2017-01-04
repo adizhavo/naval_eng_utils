af786371cc5e0504f69d86cd7ab9f657eca364dc
Adi Zhavo <adi@gram.gs>
2017-01-04 11:05:43 +0200

    Add docstrings int app.clj

--- src/naval_eng_utils/app.clj
+++ src/naval_eng_utils/app.clj
@@ -7,11 +7,13 @@
 (def original-report (doall (build-csv-hashmap config/original-file)))
 (def test-report (doall (build-csv-hashmap config/test-file)))
 
-(defn -main []
+(defn -main
+  "Loops throught all the configured actions and creates the report"
+  []
   (loop [[action & next-action] config/actions]
     (if (empty? action) 0
       (do
-        (spit (str config/output-folder action config/output-suffix) 
+        (spit (str config/output-folder action config/output-suffix)
           (op/execute action original-report test-report))
         (recur next-action)
 ))))
